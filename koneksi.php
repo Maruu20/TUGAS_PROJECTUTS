f6e5834cf677f102364d9b09cb69384009b661af
Maruu20 <namikazeamuro@gmail.com>
2026-05-28 11:12:59 +0800

    revisi login

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,14 @@
 <?php
-    session_start();
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    
+    // Cek apakah pengguna sudah login, kecuali di halaman login.php
+    $current_page = basename($_SERVER['PHP_SELF']);
+    if ($current_page !== 'login.php' && !isset($_SESSION['username'])) {
+        header("Location: login.php");
+        exit;
+    }
     
     $host = "localhost";
     $user = "root";
